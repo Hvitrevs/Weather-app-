f873cdddc54a25ea1dc2b0f58aba61caa252bc53
Hvitrevs <venusof12pm@proton.me>
2023-09-06 17:16:00 -0400

    celcius added

--- main.js
+++ main.js
@@ -44,33 +44,52 @@ function fahrenheitToCelsius(fahrenheit) {
 const currentIcon = document.querySelector("[data-current-icon]")
 
 function renderCurrentWeather(current) {
-  dailySection.innerHTML = ""
-  currentIcon.src = getIconUrl(current.iconCode)
+  dailySection.innerHTML = "";
+  currentIcon.src = getIconUrl(current.iconCode);
   const currentTempFahrenheit = current.currentTemp;
   const currentTempCelsius = fahrenheitToCelsius(currentTempFahrenheit).toFixed();
 
+  const currentHighFahrenheit = current.highTemp;
+  const currentHighCelsius = fahrenheitToCelsius(currentHighFahrenheit).toFixed();
+
+  const currentLowFahrenheit = current.lowTemp;
+  const currentLowCelsius = fahrenheitToCelsius(currentLowFahrenheit).toFixed();
+
+  const currentHighFeelsLikeFahrenheit = current.highFeelsLike;
+  const currentHighFeelsLikeCelsius = fahrenheitToCelsius(currentHighFeelsLikeFahrenheit).toFixed();
+
+  const currentLowFeelsLikeFahrenheit = current.lowFeelsLike;
+  const currentLowFeelsLikeCelsius = fahrenheitToCelsius(currentLowFeelsLikeFahrenheit).toFixed();
+
   setValue("current-temp", `${currentTempFahrenheit}°F | ${currentTempCelsius}°C`);
-  setValue("current-high", current.highTemp)
-  setValue("current-low", current.lowTemp)
-  setValue("current-fl-high", current.highFeelsLike)
-  setValue("current-fl-low", current.lowFeelsLike)
-  setValue("current-wind", current.windSpeed)
-  setValue("current-precip", current.precip)
+  setValue("current-high", `${currentHighFahrenheit}°F | ${currentHighCelsius}°C`);
+  setValue("current-low", `${currentLowFahrenheit}°F | ${currentLowCelsius}°C`);
+  setValue("current-fl-high", `${currentHighFeelsLikeFahrenheit}°F | ${currentHighFeelsLikeCelsius}°C`);
+  setValue("current-fl-low", `${currentLowFeelsLikeFahrenheit}°F | ${currentLowFeelsLikeCelsius}°C`);
+  setValue("current-wind", current.windSpeed);
+  setValue("current-precip", current.precip);
 }
 
-const DAY_FORMATTER = new Intl.DateTimeFormat(undefined, { weekday: "long" })
-const dailySection = document.querySelector("[data-day-section]")
-const dayCardTemplate = document.getElementById("day-card-template")
+const DAY_FORMATTER = new Intl.DateTimeFormat(undefined, { weekday: "long" });
+const dailySection = document.querySelector("[data-day-section]");
+const dayCardTemplate = document.getElementById("day-card-template");
 
 function renderDailyWeather(daily) {
-  dailySection.innerHTML = ""
+  dailySection.innerHTML = "";
   daily.forEach(day => {
-    const element = dayCardTemplate.content.cloneNode(true)
-    setValue("temp", day.maxTemp, { parent: element })
-    setValue("date", DAY_FORMATTER.format(day.timestamp), { parent: element })
-    element.querySelector("[data-icon]").src = getIconUrl(day.iconCode)
-    dailySection.append(element)
-  })
+    const element = dayCardTemplate.content.cloneNode(true);
+    
+    // Calculate temperature in Celsius
+    const maxTempFahrenheit = day.maxTemp;
+    const maxTempCelsius = fahrenheitToCelsius(maxTempFahrenheit).toFixed();
+    
+    // Display both temperature units
+    setValue("temp", `${maxTempFahrenheit}°F | ${maxTempCelsius}°C`, { parent: element });
+    
+    setValue("date", DAY_FORMATTER.format(day.timestamp), { parent: element });
+    element.querySelector("[data-icon]").src = getIconUrl(day.iconCode);
+    dailySection.append(element);
+  });
 }
 
 const HOUR_FORMATTER = new Intl.DateTimeFormat(undefined, { hour: "numeric" })
@@ -78,23 +97,27 @@ const hourlySection = document.querySelector("[data-hour-section]")
 const hourRowTemplate = document.getElementById("hour-row-template")
 
 function renderHourlyWeather(hourly) {
-  hourlySection.innerHTML = ""
+  hourlySection.innerHTML = "";
   const limit = 10;
   const iterations = Math.min(limit, hourly.length);
+
   for (let i = 0; i < iterations; i++) {
     const hour = hourly[i];
     const element = hourRowTemplate.content.cloneNode(true);
     const tempFahrenheit = hour.temp;
     const tempCelsius = fahrenheitToCelsius(tempFahrenheit).toFixed();
 
-    setValue("temp", `${tempFahrenheit}°F ${tempCelsius}°C`, { parent: element });
-    setValue("fl-temp", hour.feelsLike, { parent: element })
-    setValue("wind", hour.windSpeed, { parent: element })
-    setValue("precip", hour.precip, { parent: element })
-    setValue("day", DAY_FORMATTER.format(hour.timestamp), { parent: element })
-    setValue("time", HOUR_FORMATTER.format(hour.timestamp), { parent: element })
-    element.querySelector("[data-icon]").src = getIconUrl(hour.iconCode)
-    hourlySection.append(element)
+    const feelsLikeFahrenheit = hour.feelsLike;
+    const feelsLikeCelsius = fahrenheitToCelsius(feelsLikeFahrenheit).toFixed();
+
+    setValue("temp", `${tempFahrenheit}°F | ${tempCelsius}°C`, { parent: element });
+    setValue("fl-temp", `${feelsLikeFahrenheit}°F | ${feelsLikeCelsius}°C`, { parent: element });
+    setValue("wind", hour.windSpeed, { parent: element });
+    setValue("precip", hour.precip, { parent: element });
+    setValue("day", DAY_FORMATTER.format(hour.timestamp), { parent: element });
+    setValue("time", HOUR_FORMATTER.format(hour.timestamp), { parent: element });
+    element.querySelector("[data-icon]").src = getIconUrl(hour.iconCode);
+    hourlySection.append(element);
   }
 }
 
