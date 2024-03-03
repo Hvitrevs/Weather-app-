b1ace987dde5d05d0cf60ea6edaa5e19418defa5
Hvitrevs <venusof12pm@proton.me>
2024-03-03 02:50:27 -0500

    added icons

--- ICON_MAP_NIGHT.js
+++ ICON_MAP_NIGHT.js
@@ -6,7 +6,7 @@ addMapping([2], "cloud-moon")
 addMapping([3], "cloud")
 addMapping([45,48], "smog")
 addMapping([51, 53, 55, 56, 57, 61, 63, 65, 66, 67, 80, 81, 82, ], "cloud-showers-heavy")
-addMapping([71, 73, 75, 77, 85, 86], "snowflake")
+addMapping([71, 73, 75, 77, 85, 86], "night-snow")
 addMapping([95, 96, 99], "cloud-bolt")
 
 function addMapping(values, icon){
